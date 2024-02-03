2c1cd6992b3c4d47d46a9fe97295b7685bb33a6b
Redevil15 <98137974+Redevil15@users.noreply.github.com>
2024-02-02 19:09:00 -0600

    Feat: add loading component

--- providers/convex-client-provider.tsx
+++ providers/convex-client-provider.tsx
@@ -7,6 +7,7 @@ import {
   Authenticated,
   ConvexReactClient
 } from "convex/react";
+import { Loading } from "@/components/auth/loading";
 
 interface ConvexClientProviderProps {
   children: React.ReactNode;
@@ -29,6 +30,9 @@ export const ConvexClientProvider = ({ children }: ConvexClientProviderProps) =>
         <Authenticated>
           {children}
         </Authenticated>
+        <AuthLoading>
+          <Loading/>
+        </AuthLoading>
       </ConvexProviderWithClerk>
     </ClerkProvider>
   );
